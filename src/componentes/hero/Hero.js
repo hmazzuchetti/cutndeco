cfe3f85eee5189d586fc796abdf13f77a4106dca
hmazzuchetti <henrique.mazzu@hotmail.com>
2021-11-16 01:09:26 -0300

    Alteracoes card natalino e algumas cores

--- src/componentes/hero/Hero.js
+++ src/componentes/hero/Hero.js
@@ -3,16 +3,21 @@ import "./hero.css";
 
 function Hero() {
     return (
-        <div className="hero">
-            <div className="bckgd"> {/*CAPA DE FUNDO*/} </div>
+        <>
+            <div className="hero">
+                <div className="bckgd"> {/*CAPA DE FUNDO*/} </div>
 
-            <h1>O natal já chegou</h1>
-            <h2>Confira os presentes que preparamos para você</h2>
-            <div className="btns">
-                <button className="btnHero"><a>Ver produtos</a></button>
-                <button className="btnHero"><a>Contato</a></button>
+                <h1>O natal já chegou</h1>
+                <h2>Confira os presentes que preparamos para você</h2>
+                <div className="btns">
+                    <button className="btnHero"><a>Ver produtos</a></button>
+                    <button className="btnHero"><a>Contato</a></button>
+                </div>
             </div>
-        </div>
+
+            <svg className="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(248, 218, 223)" fill-opacity="1" d="M0,224L60,208C120,192,240,160,360,154.7C480,149,600,171,720,192C840,213,960,235,1080,240C1200,245,1320,235,1380,229.3L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+
+        </>
     )
 }
 
--- src/componentes/produtoNatal/ProdutoNatal.js
+++ src/componentes/produtoNatal/ProdutoNatal.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import "./produtonatal.css";
+import natal1 from "../../img/Natal/natal1Quadrado.jpg";
+import natal2 from "../../img/Natal/natal2Quadrado.jpg";
+import natal3 from "../../img/Natal/natal3Quadrado.jpg";
+import natal4 from "../../img/Natal/natal4Quadrado.jpg";
 
 function ProdutoNatal() {
     return (
         <div className="container">
+
             <div className="texto__produtoNatal">
-                <h1>Lembrancinhas de natal</h1>
-                <p>Não sabe o que dar de presente? Não se preocupe, nós já pensamos nisso para você. Entre em contato e conheça nossas caixinhas que estão uma gracinha.</p>
+
+                <p className="pGrande">Procurando presente?</p>
+                <p className="pPequeno"> Veja nossas caixinhas de natal!</p>
                 <div className="btns">
-                    <button className="btnCaixa">Comprar</button>
+                    <button className="btnCaixa"><p>Fazer pedido</p></button>
+                    <button className="btnCaixa"><p>Ver fotos</p></button>
                 </div>
+                <div className="cardBckgd"></div>
             </div>
-            <div className="foto__produto"></div>
+            <div className="foto__produto">
+                <img className="imgnatal" alt="Produto Natal 1" src={natal1} />
+                <img className="imgnatal" alt="Produto Natal 1" src={natal2} />
+                <img className="imgnatal" alt="Produto Natal 1" src={natal3} />
+                <img className="imgnatal" alt="Produto Natal 1" src={natal4} />
+            </div>
         </div>
     )
 }
